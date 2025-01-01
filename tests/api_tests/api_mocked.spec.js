e834d5176fe346e4df592702eea583839c3addbb
Anton Kolupaiev <antnikol@gmail.com>
2025-01-01 23:21:28 +0200

    ref mocked test

--- tests/api_tests/api_mocked.spec.js
+++ tests/api_tests/api_mocked.spec.js
@@ -6,16 +6,19 @@ const { message, searchTerms, user, userUpdate } = jsonData
 test.describe('API tests with mocked data', () => {
 
   test('API_ 1(3): __Mocked_DATA__ Get All Products List', async ({ page, request }) => {
-    await page.route('**/api/productsList', route => {
+    await page.route('**/api/productsList', (route) => {
       route.fulfill({
         status: 201,
-        body: JSON.stringify({ products: [{ name: 'mocked' }] })
-      })
-    })
-  
-    await page.goto('http://localhost:3000/mockPage.html')
-    const response = await page.waitForResponse('**/api/productsList')
+        body: JSON.stringify({ products: [{ name: 'mocked' }] }),
+      });
+    });
+
+    await page.goto('http://127.0.0.1:55547/mockPage.html');
+    const response = await page.waitForResponse((response) =>
+      response.url().includes('/api/productsList')
+    )
     const responseBody = await response.json()
+    
     expect(response.status()).toBe(201)
     expect(responseBody.products[0].name).toBe('mocked')
   })
