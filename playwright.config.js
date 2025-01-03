ce8d3866b5e0e65d7859acb951df55fb0c86957f
Anton Kolupaiev <antnikol@gmail.com>
2025-01-03 22:57:03 +0200

    add new tests to test history + change to 1 try on CI

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 5 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 1,
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }], 
