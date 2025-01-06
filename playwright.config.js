3207fb6591cf013bd961018a5166d8eaef799839
Anton Kolupaiev <antnikol@gmail.com>
2025-01-06 21:54:34 +0200

    try on 1 worker

--- playwright.config.js
+++ playwright.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 15 : 5,
-  workers: process.env.CI ? 2 : undefined,
+  retries: process.env.CI ? 30 : 5,
+  workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }], 
     ['list'],
